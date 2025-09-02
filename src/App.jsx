4a7a7de5dcd6f3373df3ff4ec73cd976385c2ad2
Leyla16125 <lhuseynova16125@ada.edu.az>
2025-09-03 00:50:42 +0400

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./src/components/pages/Home";
-import About from "./src/components/pages/About";
-import CommonLayout from "./layout/CommonLayout";
-import Login from "./src/components/pages/Login";
-import Cars from "./src/components/pages/Cars";
-import CarDetails from "./src/components/pages/CarDetails";
-import Contact from "./src/components/pages/Contact";
-import Like from "./src/components/pages/Like";
-import Documents from "./src/components/pages/Documents"
-import Logistics from "./src/components/srvcs/Logistics";
-import VehicleInspection from "./src/components/srvcs/VehicleInspection";
-import VehicleSearch from "./src/components/srvcs/VehicleSearch";
-import { LikeProvider } from "./src/components/pages/LikeContext";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import CommonLayout from "../layout/CommonLayout";
+import Login from "./components/pages/Login";
+import Cars from "./components/pages/Cars";
+import CarDetails from "./components/pages/CarDetails";
+import Contact from "./components/pages/Contact";
+import Like from "./components/pages/Like";
+import Documents from "./components/pages/Documents"
+import Logistics from "./components/srvcs/Logistics";
+import VehicleInspection from "./components/srvcs/VehicleInspection";
+import VehicleSearch from "./components/srvcs/VehicleSearch";
+import { LikeProvider } from "./components/pages/LikeContext";
 
 function App() {
   return (
